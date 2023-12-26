613fc2143256067b3f674f0a9ff57c6e3128f1af
kholiavkoi <kholiavkoi@gmail.com>
2023-12-26 20:47:06 +0200

    added mongoose and created user and product schema

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,10 +1,13 @@
+import { fetchUsers } from "@/app/lib/data";
 import Pagination from "@/app/ui/dashboard/pagination/Pagination";
 import Search from "@/app/ui/dashboard/search/Search";
 import styles from "@/app/ui/dashboard/users/Users.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-const Users = () => {
+const Users = async () => {
+  const users = await fetchUsers();
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -25,36 +28,38 @@ const Users = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src="/noavatar.png"
-                  alt="avatar"
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                John Doe
-              </div>
-            </td>
-            <td>john@gmail.com</td>
-            <td>13.01.2024</td>
-            <td>Admin</td>
-            <td>Active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href="/dashboard/users/test">
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img || "/noavatar.png"}
+                    alt="avatar"
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  John Doe
+                </div>
+              </td>
+              <td>john@gmail.com</td>
+              <td>13.01.2024</td>
+              <td>Admin</td>
+              <td>Active</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href="/dashboard/users/test">
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
                   </button>
-                </Link>
-                <button className={`${styles.button} ${styles.delete}`}>
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       <Pagination />
