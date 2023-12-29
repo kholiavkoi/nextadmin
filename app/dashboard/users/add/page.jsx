e4a51ebafceead7c884df62ce254aa7c0cbf3ad9
kholiavkoi <kholiavkoi@gmail.com>
2023-12-29 12:17:10 +0200

    added server actions to add users and products

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -1,9 +1,10 @@
+import { addUser } from "@/app/lib/actions";
 import styles from "@/app/ui/dashboard/users/addUser/AddUser.module.css";
 
 const AddUserPage = () => {
   return (
     <div className={styles.container}>
-      <form action="" className={styles.form}>
+      <form action={addUser} className={styles.form}>
         <input type="text" placeholder="username" name="username" required />
         <input type="email" placeholder="email" name="email" required />
         <input
@@ -14,16 +15,17 @@ const AddUserPage = () => {
         />
         <input type="phone" placeholder="phone" name="phone" />
         <select name="isAdmin" id="isAdmin">
-          <option defaultValue={false}>Is Admin?</option>
+          <option value={false}>Is Admin?</option>
           <option value={true}>Yes</option>
           <option value={false}>No</option>
         </select>
 
         <select name="isActive" id="isActive">
-          <option defaultValue={true}>Is Active?</option>
+          <option value={true}>Is Active?</option>
           <option value={true}>Yes</option>
           <option value={false}>No</option>
         </select>
+
         <textarea
           name="address"
           id="address"
