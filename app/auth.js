dae29ac42f7133a320a4af9d8f076bcf5341497c
kholiavkoi <kholiavkoi@gmail.com>
2024-01-05 10:47:40 +0200

    done authentication

---
+++ app/auth.js
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./authconfig";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDB } from "./lib/utils";
+import { User } from "./lib/models";
+import bcrypt from "bcrypt";
+
+const login = async (credentials) => {
+  try {
+    connectToDB();
+    const user = await User.findOne({ username: credentials.username });
+    if (!user) throw new Error("Wrong credentials!");
+
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect) throw new Error("Wrong credentials!");
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to login!");
+  }
+};
+
+export const { signIn, signOut, auth } = NextAuth({
+  ...authConfig,
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        try {
+          const user = await login(credentials);
+          return user;
+        } catch (error) {
+          console.log(error);
+          throw new Error("Failed to login!");
+        }
+      },
+    }),
+  ],
+});
+
--- app/lib/actions.js
+++ app/lib/actions.js
@@ -4,6 +4,7 @@ import { Product, User } from "./models";
 import { connectToDB } from "./utils";
 import { redirect } from "next/navigation";
 import bcrypt from "bcrypt";
+import { signIn } from "../auth";
 
 export const addUser = async (formData) => {
   const { username, email, password, phone, address, isAdmin, isActive } =
@@ -148,4 +149,15 @@ export const deleteProduct = async (formData) => {
   }
 
   revalidatePath("dashboard/products");
+};
+
+export const authenticate = async (formData) => {
+  const { username, password } = Object.fromEntries(formData);
+
+  try {
+    await signIn("credentials", { username, password });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
